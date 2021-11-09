ebd00c7314c5ac591853cf33cf29376057a6f9a7
Travis Dever <tdever@aviatrix.com>
2021-11-09 09:29:18 -0600

    adding aviatrix controller pipeline and terraform module

---
+++ terraform/controller/backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "azurerm" {
+    key                  = "controller.deployment.state"
+    use_azuread_auth     = true
+  }
+}
+
+data "terraform_remote_state" "initial_config" {
+  backend = "azurerm"
+  config = {
+    storage_account_name = var.state_storage_account_name
+    container_name       = "tfstate"
+    key                  = "initial.config.state"
+    use_azuread_auth     = true
+  }
+}
