12de86e8dc96345fc37fedf9a01fc1a38f2c91c7
Travis Dever <tdever@aviatrix.com>
2021-11-09 12:53:00 -0600

    adding output for aviatrix account

--- terraform/controller/outputs.tf
+++ terraform/controller/outputs.tf
@@ -9,3 +9,9 @@ output "controller_public_ip" {
   description = "The IP Address of the Aviatrix Controller"
   sensitive = true
 }
+
+output "aviatrix_azure_account" {
+  value       = module.azure_aviatrix_controller.aviatrix_azure_account
+  description = "The Azure account provisioned in the aviatrix controller used for accessing subscriptions"
+  sensitive = true
+}
