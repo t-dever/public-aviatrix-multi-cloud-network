637f50bfed45a6ed4259b1ff771e25e08895637a
Travis Dever <tdever@aviatrix.com>
2021-11-09 12:31:26 -0600

    adding build agent ip address for controller

--- terraform/controller/variables.tf
+++ terraform/controller/variables.tf
@@ -3,4 +3,9 @@ variable "state_storage_account_name" {
 }
 variable "user_public_ip_address" {
   description = "The users public IP Address to access the controller."
+  sensitive   = true
+}
+variable "build_agent_ip_address" {
+  description = "The Public IP Address of the build agent to add to the NSG allow rule"
+  sensitive   = true
 }
