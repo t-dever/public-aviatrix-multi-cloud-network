da15689c2513ca0c260a3ef173aa3e1090cfb346
Travis Dever <tdever@aviatrix.com>
2021-12-07 10:01:39 -0600

    updating python script to generate yaml pipelines

--- scripts/generate_pipelines.py
+++ scripts/generate_pipelines.py
@@ -1,54 +1,69 @@
 import os
-from ruamel.yaml import YAML
+from ruamel.yaml import YAML, comments
 from os.path import isfile, realpath, isdir
+from io import StringIO
 
 root_path = os.path.dirname(realpath(__name__))
 
 environments = ['dev', 'lab']
 yaml=YAML()
+yaml.indent(mapping=2, sequence=2, offset=0)
 
-def update_transit_pipeline(pipeline_file, cloud_provider, regions):
+def object_to_yaml_str(obj, options=None):
+    if options == None: options = {}
+    string_stream = StringIO()
+    yaml.dump(obj, string_stream, **options)
+    output_str = string_stream.getvalue()
+    string_stream.close()
+    return output_str
+
+def get_regions(terraform_path, csp):
+    file_path = f"{terraform_path}/{csp}"
+    if isdir(file_path):
+        regions = os.listdir(file_path)
+        return regions
+    else:
+        return None
+
+def update_pipeline(pipeline_file, env, terraform_path, azure_regions=None, aws_regions=None):
     stream = open(pipeline_file, 'r')
     data = yaml.load(stream)
-    for stage in data['stages']:
+    for index, stage in enumerate(data['stages']):
         if stage['template'] == "templates/stages/transits.yml":
-            if cloud_provider == "azure":
-                stage['parameters']['azureRegions'] = regions
-    print(data)
-    with open(pipeline_file, 'w') as f:
-        f.write(yaml.dump(data, f))
-        f.close()
-
-def update_spoke_pipeline(pipeline_file, env, cloud_provider, regions):
-    terraform_path = f"{root_path}/terraform/{env}/{cloud_provider}"
-    combined_spokes = []
-    for region in regions:
-        spoke_path = f"{terraform_path}/{region}/spokes"
-        if isdir(spoke_path):
-            spoke_params = {
-                'csp': cloud_provider,
-                'region': region,
-                'spokes': []
-            }
-            spokes = os.listdir(spoke_path)
-            for spoke in spokes:
-                spoke_params['spokes'].append(spoke)
-            combined_spokes.append(spoke_params)
-    print(combined_spokes)
-    stream = open(pipeline_file, 'r')
-    data = yaml.load(stream)
-    for stage in data['stages']:
+            combined_transits = []
+            if azure_regions:
+                for region in azure_regions:
+                    combined_transits.append(
+                        {
+                            'csp': 'azure',
+                            'region': region
+                        })
+            stage['parameters']['transits'] = combined_transits
         if stage['template'] == "templates/stages/spokes.yml":
+            data['stages'][index].yaml_set_start_comment('\n')
+            combined_spokes = []
+            if azure_regions:
+                for region in azure_regions:
+                    spoke_path = f"{terraform_path}/azure/{region}/spokes"
+                    if isdir(spoke_path):
+                        spoke_params = {
+                            'csp': 'azure',
+                            'region': region,
+                            'spokes': []
+                        }
+                        spokes = os.listdir(spoke_path)
+                        for spoke in spokes:
+                            spoke_params['spokes'].append(spoke)
+                            combined_spokes.append(spoke_params)
             stage['parameters']['spokes'] = combined_spokes
+    data = object_to_yaml_str(data)
     with open(pipeline_file, 'w') as f:
-        f.write(yaml.dump(data, f))
+        f.write(data)
         f.close()
 
 for env in environments:
     pipeline = f"{root_path}/pipelines/{env}.pipeline.yml"
     terraform_path = f"{root_path}/terraform/{env}"
-    azure_path = f"{terraform_path}/azure"
-    if isdir(azure_path):
-        azure_regions = os.listdir(azure_path)
-        update_transit_pipeline(pipeline, 'azure', azure_regions)
-        update_spoke_pipeline(pipeline, env, 'azure', azure_regions)
+    azure_regions = get_regions(terraform_path, 'azure')
+    if isfile(pipeline):
+        update_pipeline(pipeline, env, terraform_path, azure_regions=azure_regions)
